71f21bbbdc71301c1d39befb13675951397b40e6
godwing121 <sagayamgodwin19@gmail.com>
2024-10-29 21:20:10 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "godwin"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "god"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "GW"
 )
 
 var ID ids.ID
